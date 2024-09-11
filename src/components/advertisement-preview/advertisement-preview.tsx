4d866707f04f0a20960c85d0243f254ac370a3d5
Dinara Dulanova <dinara.dulanova@yandex.ru>
2024-09-11 17:00:18 +0300

    feature:отображение объявлени из заказа в модалке

--- src/components/advertisement-preview/advertisement-preview.tsx
+++ src/components/advertisement-preview/advertisement-preview.tsx
@@ -5,25 +5,16 @@ import './advertisement-preview.css';
 
 export const AdvertisementPreview: FC<{ advertisement: TAdvertisment }> = ({
   advertisement
-}) => {
-  const location = useLocation();
-  return (
-    <div className='advertisement-card'>
-      <img
-        className='advertisement-card__img'
-        src={advertisement.imageUrl}
-        alt={`img of ${advertisement.name}`}
-      />
-      <h3>{advertisement.name}</h3>
-      <p>{advertisement.price}</p>
-      <p>{advertisement.views}</p>
-      <p>{advertisement.likes}</p>
-    </div>
-  );
-};
-
-// export const AdvertisementCard: FC<{ advertisement: TAdvertisment }> = ({
-//   advertisement
-// }) => {
-//   return <div>{advertisement.name}</div>;
-// };
+}) => (
+  <div className='advertisement-card'>
+    <img
+      className='advertisement-card__img'
+      src={advertisement.imageUrl}
+      alt={`img of ${advertisement.name}`}
+    />
+    <h3>{advertisement.name}</h3>
+    <p>{advertisement.price}</p>
+    <p>{advertisement.views}</p>
+    <p>{advertisement.likes}</p>
+  </div>
+);
--- src/components/order-card/order-card.tsx
+++ src/components/order-card/order-card.tsx
@@ -1,24 +1,62 @@
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Modal } from '../modal/modal';
+import { AdvertisementPreview } from '../advertisement-preview';
 import { renderStatusToString } from '../../utils/helpers/functions';
 import './order-card.css';
 
-export const OrderCard: FC<{ order: TOrder }> = ({ order }) => (
-  <>
+export const OrderCard: FC<{ order: TOrder }> = ({ order }) => {
+  // Modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
     <div className='order-card'>
-      <div>Количество товаров: {order.items.length}</div>
-      <div>Стоимость заказа: {order.total}</div>
-      <div>Статус заказа: {renderStatusToString(order.status)}</div>
-      <div>
-        Дата создания заказа: {new Date(order.createdAt).toLocaleDateString()}
+      <div className='order-card__header'>
+        <div>Номер заказа: {order.id}</div>
+        <div>Статус заказа: {renderStatusToString(order.status)}</div>
       </div>
-      <div>Номер заказа: {order.id}</div>
+      <div className='order-card__content'>
+        <div className='order-card__item'>
+          <span>Количество товаров:</span> {order.items.length}
+        </div>
+        <div className='order-card__item'>
+          <span>Стоимость заказа:</span> {order.total}
+        </div>
+        <div className='order-card__item'>
+          <span>Дата создания:</span>{' '}
+          {new Date(order.createdAt).toLocaleDateString()}
+        </div>
+      </div>
+      <button onClick={handleModalOpen} className='order-card__button'>
+        Показать все товары
+      </button>
+
+      {isModalOpen && (
+        <>
+          <Modal
+            title={`Объявления из заказа номер ${order.id}`}
+            onClose={handleModalClose}
+          >
+            <div className='order-card__modal-content'>
+              {order.items.map((item) => (
+                <Link key={item.id} to={`/advertisement/${item.id}`}>
+                  <AdvertisementPreview advertisement={item} />
+                </Link>
+              ))}
+            </div>
+          </Modal>
+        </>
+      )}
     </div>
-  </>
-);
+  );
+};
 
 // Возможность завершения заказа;
-// ;
-// Статус (текстом);
-// ;
 // vКнопка “Показать все товары”, показывающая все товары в данном заказе (можно отображать их в этой же карточке или сделать модальное окно)
