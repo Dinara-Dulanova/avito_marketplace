3db5bf78bd944d3b511199dbb2b1480a746d4189
Dinara Dulanova <dinara.dulanova@yandex.ru>
2024-09-12 23:43:53 +0300

    feature: READme, стили

--- src/components/add-advertisement-modal/add-advertisement-modal.tsx
+++ src/components/add-advertisement-modal/add-advertisement-modal.tsx
@@ -1,7 +1,9 @@
 import { TNewAdvertisment } from '@utils-types';
 import React, { useState } from 'react';
+import { Button, Input } from '@mui/material';
 import { useDispatch } from '../../services/store';
 import { postNewAdvertisement } from '../../services/slices/advertisements';
+import './style.css';
 
 interface AddAdvertisementModalProps {
   onClose: () => void;
@@ -41,45 +43,52 @@ export const AddAdvertisementModal: React.FC<AddAdvertisementModalProps> = ({
     <>
       {!isSuccesfullyAdded && (
         <>
-          <h2 className='modal-title'>Новое Объявление</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor='name'>Название:</label>
-              <input
-                type='text'
-                id='name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor='imageUrl'>Картинка (URL):</label>
-              <input
-                type='text'
-                id='imageUrl'
-                value={imageUrl}
-                onChange={(e) => setImageUrl(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor='description'>Описание:</label>
-              <textarea
-                id='description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor='price'>Стоимость:</label>
-              <input
-                type='number'
-                id='price'
-                value={price}
-                onChange={(e) => setPrice(Number(e.target.value))}
-              />
-            </div>
-            <button type='submit'>Добавить</button>
-          </form>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className='form-group'>
+                <label htmlFor='name'>Название:</label>
+                <Input
+                  required
+                  type='text'
+                  id='name'
+                  value={name}
+                  placeholder='Название объявления'
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='imageUrl'>Картинка (URL):</label>
+                <Input
+                  type='text'
+                  id='imageUrl'
+                  value={imageUrl}
+                  placeholder='URL картинки'
+                  onChange={(e) => setImageUrl(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='description'>Описание:</label>
+                <textarea
+                  id='description'
+                  value={description}
+                  placeholder='Описание'
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='price'>Стоимость:</label>
+                <Input
+                  required
+                  type='number'
+                  id='price'
+                  value={price}
+                  placeholder='Стоимость'
+                  onChange={(e) => setPrice(Number(e.target.value))}
+                />
+              </div>
+              <Button type='submit'>Добавить</Button>
+            </form>
+          </div>
         </>
       )}
 
